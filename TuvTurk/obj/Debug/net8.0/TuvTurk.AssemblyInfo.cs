65efabab2e4c3ab969d676b6fd855f3cdb51ab08
cosmos <56826863+cosmos-droid@users.noreply.github.com>
2026-01-04 18:52:47 +0300

    db change

--- TuvTurk/obj/Debug/net8.0/TuvTurk.AssemblyInfo.cs
+++ TuvTurk/obj/Debug/net8.0/TuvTurk.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TuvTurk")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20ad57edbbad188f82ae4c2ef7fc228db3c0a91e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e89a20c6f4f65adf8bccffe99ecd6f44bab30795")]
 [assembly: System.Reflection.AssemblyProductAttribute("TuvTurk")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TuvTurk")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
