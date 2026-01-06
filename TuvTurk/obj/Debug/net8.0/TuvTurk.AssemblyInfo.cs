d9df5b0bd71e2beb5fd02600dcee96cae325c528
cosmos <56826863+cosmos-droid@users.noreply.github.com>
2026-01-06 23:01:59 +0300

    k

--- TuvTurk/obj/Debug/net8.0/TuvTurk.AssemblyInfo.cs
+++ TuvTurk/obj/Debug/net8.0/TuvTurk.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TuvTurk")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65efabab2e4c3ab969d676b6fd855f3cdb51ab08")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+855869c59d2faa85ccfcb3394124e65bf3965a9f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TuvTurk")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TuvTurk")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
